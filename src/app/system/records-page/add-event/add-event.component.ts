c4005100be87060e08ee219aea298cb4e175bd6a
Andrey_programmer <lak0sta1302@gmail.com>
2018-10-15 20:32:17 +0300

    пДуыLesson 22.

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import * as moment from 'moment';
+
+import { MyEvent } from '../../shared/models/event.model';
+import { Category } from '../../shared/models/category.model';
 
 @Component({
   selector: 'block-add-event',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-event.component.scss']
 })
 export class AddEventComponent implements OnInit {
+  @Input() categories: Category[] = [];
+
+  types = [
+    {
+    type: 'income', label: 'Доход'
+    },
+    {
+    type: 'outcome', label: 'Расход'
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    const  {amount, description, category, type} = form.value;
+    const _amount = Math.abs(amount);
+
+    const event = new MyEvent(type, _amount, +category, moment().format('DD.MM.YYYY HH:mm:ss'), description);
   }
 
 }
