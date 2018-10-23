7113ba9e5113d2b4b0fd6e47dcae0790bd25a4bc
Andrey_programmer <lak0sta1302@gmail.com>
2018-10-23 15:29:52 +0300

    Ещё вариант роутингов, но Ленивой загрузки нет

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     AuthModule,
-    SystemModule,
+    // SystemModule,
     HttpClientModule,
     RouterModule.forRoot(routes,
                         {
